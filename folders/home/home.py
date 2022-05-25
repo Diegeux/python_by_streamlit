d0f60796ea365269e59c8f6be0b4b82ddade6bc8
Diego Oliveira <diego.ijcpm2017@gmail.com>
2022-05-25 10:32:51 -0300

    requirements alterado 788

--- folders/home/home.py
+++ folders/home/home.py
@@ -5,7 +5,7 @@ def start_home():
     st.write("DOA Analytics & Prediction System")
     st.write("-Present and Future through one click-")
     st.write("This is a Business Intelligence System which is able to show you data in the present and in the future to provide insights to make decisions!")
-    image = Image.open(r'folders\home\img.jpg')
+    image = Image.open('folders/home/img.jpg')
     st.image(image, caption='')
     
       
